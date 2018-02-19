67ee7dd2d2d5a524325d6d800203816a1fc3e008
SE HEE AHN <seheeahn@ses-macbook-pro.wifi.bcit.ca>
2018-02-19 14:04:07 -0800

    123
    123

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const server = require("http").Server();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 var io = require("socket.io")(server);
 
 var usernames = [];
 var msgs = [];
+var allRooms ={};
+
 
 io.on("connection", function(socket){
     
@@ -18,6 +20,24 @@ io.on("connection", function(socket){
         
     })
     
+    socket.on("joinroom", function(data){
+        socket.emit("yourid", socket.id);
+        socket.join(data);
+        //io.emit("createimage", allusers);
+        socket.myRoom =data;
+        
+        if(!allRooms[data]){
+            allRooms[data] = [];
+        }
+        
+        
+        allRooms[data].push(socket.id);
+        io.to(data).emit("createimage", allRooms[data]);
+    
+           console.log(data);
+        
+    });
+        
     socket.on("sendChat", function(data){
         console.log("user sent a msg for chat");
         msgs.push(data);
@@ -25,7 +45,14 @@ io.on("connection", function(socket){
         io.emit("msgsent", msgs);
     });
     
+    socket.on("mymove", function(data){
+        socket.to(this.myRoom).emit("usermove", data); 
+    });
+    
     socket.on("disconnect", function(){
+        var index = allRooms[this.myRoom].indexOf(socket.id);
+        allRooms[this.myRoom].splice(index, 1);
+        io.to(this.myRoom).emit("createimage", allRooms[this.myRoom]);
         console.log("user has disconnected");
     })
     
